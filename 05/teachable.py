bfdb72f8ccb59800ae7c042519844c6e3ae31499
szeidan <szeidan@umich.edu>
2025-10-11 21:37:30 -0400

    adding README

--- 05/teachable.py
+++ 05/teachable.py
@@ -17,7 +17,7 @@ class_names = open("model/labels.txt", "r").readlines()
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open("images/scissors.jpg").convert("RGB")
+image = Image.open("images/rock.jpg").convert("RGB")
 
 
 # resizing the image to be at least 224x224 and then cropping from the center
